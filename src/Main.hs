beb0ddbb445897126702f77a914363512b36706b
Eric Litak <elitak@gmail.com>
2014-06-15 08:07:01 -0700

    simple filelisting

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
-import Prelude hiding (readFile)
 import Data.BEncode
-import Data.ByteString.Lazy hiding (putStrLn)
---import Data.Maybe
+import Data.ByteString.Lazy as BS
+import Data.Map
+import Control.Monad
+import Prelude as P
 
 main = do
-    torrent <- readFile "sample.torrent"
-    let Just info = bRead torrent
-    print info
+    torrent <- BS.readFile "sample.torrent"
+    let Just bdata = bRead torrent
+    let BDict outer = bdata
+    let BDict info = outer ! "info"
+    let BList files = info ! "files"
+    forM files $ \(BDict fileInfo) -> do
+        let BList [BString path] = fileInfo ! "path"
+        let BInt length = fileInfo ! "length"
+        P.putStr $ show length ++ " "
+        BS.putStrLn path
